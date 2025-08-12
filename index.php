d91d577f71a151fcf6bfdec75a7ad52c81c38bbe
Dalay20 <137733175+Dalay20@users.noreply.github.com>
2025-08-12 02:17:15 +0000

    Estadistica

--- index.php
+++ index.php
@@ -58,42 +58,47 @@ $denuncias = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <a href="index.php">Quitar filtros</a>
     </form>
 
+    <!-- Botón para ver estadísticas -->
+    <p>
+        <a href="reporte.php">Estadísticas</a>
+    </p>
+
     <div id="map"></div>
 
-<!-- Lista de denuncias -->
-<h2>Listado de denuncias</h2>
-<?php if ($denuncias): ?>
-    <table border="1" cellpadding="5" cellspacing="0">
-        <tr>
-            <th>Tipo</th>
-            <th>Ubicación</th>
-            <th>Descripción</th>
-            <th>Fecha</th>
-            <th>Foto</th>
-            <th>Acciones</th>
-        </tr>
-        <?php foreach ($denuncias as $d): ?>
-        <tr>
-            <td><?php echo htmlspecialchars($d['tipo']); ?></td>
-            <td><?php echo htmlspecialchars($d['ubicacion']); ?></td>
-            <td><?php echo htmlspecialchars($d['descripcion']); ?></td>
-            <td><?php echo $d['fecha']; ?></td>
-            <td>
-                <?php if ($d['foto']): ?>
-                    <img src="<?php echo $d['foto']; ?>" width="80">
-                <?php else: ?>
-                    Sin foto
-                <?php endif; ?>
-            </td>
-            <td>
-                <a href="comentario.php?id=<?php echo $d['id']; ?>">Ver comentarios</a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-<?php else: ?>
-    <p>No hay denuncias para mostrar.</p>
-<?php endif; ?>
+    <!-- Lista de denuncias -->
+    <h2>Listado de denuncias</h2>
+    <?php if ($denuncias): ?>
+        <table border="1" cellpadding="5" cellspacing="0">
+            <tr>
+                <th>Tipo</th>
+                <th>Ubicación</th>
+                <th>Descripción</th>
+                <th>Fecha</th>
+                <th>Foto</th>
+                <th>Acciones</th>
+            </tr>
+            <?php foreach ($denuncias as $d): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($d['tipo']); ?></td>
+                <td><?php echo htmlspecialchars($d['ubicacion']); ?></td>
+                <td><?php echo htmlspecialchars($d['descripcion']); ?></td>
+                <td><?php echo $d['fecha']; ?></td>
+                <td>
+                    <?php if ($d['foto']): ?>
+                        <img src="<?php echo $d['foto']; ?>" width="80">
+                    <?php else: ?>
+                        Sin foto
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <a href="comentario.php?id=<?php echo $d['id']; ?>">Ver comentarios</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php else: ?>
+        <p>No hay denuncias para mostrar.</p>
+    <?php endif; ?>
 
 
 
